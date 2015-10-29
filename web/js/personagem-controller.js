8a92cc7c5cece01fc533b171381426e278737fd1
Rafael Lossurdo <lossurdo@gmail.com>
2015-10-29 17:14:34 -0200

    criada entidade personagem

--- web/js/personagem-controller.js
+++ web/js/personagem-controller.js
@@ -1,12 +1,12 @@
-var app = angular.module('gotApp', []);
-app.controller('GotController', function ($scope, $http) {
+var app = angular.module('personagemApp', []);
+app.controller('PersonagemController', function ($scope, $http) {
     
     $scope.salvar = function () {
         console.log($scope.fields);
         $http({
             method: 'POST',
             data: $scope.fields,
-            url: '/RESTful_CRUD/rest/got',
+            url: '/RESTful_CRUD/rest/personagens',
             headers: {'Content-Type': 'application/json'}
         }).success(function (data, status, headers, config) {
             console.log(data);
@@ -18,21 +18,21 @@ app.controller('GotController', function ($scope, $http) {
     };
         
     $scope.todos = function() {
-        $http.get('/RESTful_CRUD/rest/got').success(function (data) {
-            $scope.got = data;
+        $http.get('/RESTful_CRUD/rest/personagens').success(function (data) {
+            $scope.personagens = data;
             $scope.existemDados = true;
         });
     };
 
-    $scope.consultar = function(got) {
-        $http.get('/RESTful_CRUD/rest/got/' + got.codigo).success(function (data) {
+    $scope.consultar = function(personagem) {
+        $http.get('/RESTful_CRUD/rest/personagens/' + personagem.codigo).success(function (data) {
             console.log(data);
             $scope.fields = data;
         });
     };
 
-    $scope.excluir = function(got) {
-        $http.delete('/RESTful_CRUD/rest/got/' + got.codigo).success(function (data) {
+    $scope.excluir = function(personagem) {
+        $http.delete('/RESTful_CRUD/rest/personagens/' + personagem.codigo).success(function (data) {
             console.log(data);
             $scope.fields = data;
             $scope.todos();
